a504e3489d54047772120072c349a13794053bb4
dubo <dubo@sinosoft.com.cn>
2018-10-25 13:38:11 +0800

    添加vue-demo

---
+++ web/src/components/memu/memu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="memu">
+    <h1>{{msg}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.name}}</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Memu",
+    data() {
+      return {
+        msg: "hello memu",
+        books: [
+          {name: '<<book01>>'},
+          {name: '<<book02>>'},
+          {name: '<<book03>>'},
+          {name: '<<book04>>'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
